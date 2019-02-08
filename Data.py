442f5e2e04f2148f4b8fbfb966e737cfc776f56f
Rhydik <jacob.bergqvist@gmail.com>
2019-02-08 23:46:30 +0100

    Decision Tree complete. Moving on to random forest

--- Data.py
+++ Data.py
@@ -2,63 +2,17 @@ import pandas as pd
 import Tree as t
 import numpy as np
 
-class Dataset:
-    def __init__(self):
-        return
-
-    def getLabels(self, data):
-        labels = list(data)
-        myarray = np.asarray(labels)
-        return myarray
-
-    def printData(self, dataset):
-        print(dataset)
-
-    def loadData(self, dataset):
-        data = pd.read_csv(dataset)
-        return data
-
-if __name__ == '__main__':
-    ds = Dataset()
-
-    data = ds.loadData("Datasets/binary/balance-scale.csv")
 
 
-    array = data.values.tolist()
-    rootnode = t.Node(array)
-    ds.printData(data)
-    labels = ds.getLabels(data)
+def getLabels(data):
+    labels = list(data)
+    myarray = np.asarray(labels)
+    return myarray
 
-    #print(array[0])
-    tree = t.Tree('Gini')
-    #q1 = t.Question(1, 5, labels)
+def printData(dataset):
+    print(dataset)
 
-    #rows = q1.class_counts(data)
-
-    #true_rows = rows.pop()
-    #print(q1.class_counts(rootnode.dataset))
-    #print(q.match(array[1]))
-    #true_row1, false_row1 = tree.partition(rootnode.dataset, q1)
-    #control = t.Question(4, 'R', labels)
-    #print(control.match(true_row1))
-    #lchild1 = t.Node(true_row1)
-    #q2 = t.Question(1, 1, labels)
-    #true_row2, false_row2 = tree.partition(rootnode.dataset, q1)
-    #rchild1 = t.Node(false_row1)
-
-
-    gain, question = tree.find_split(rootnode.dataset, labels)
-
-    print(question)
-    #print(tree.entropy(true_row1, false_row1))
-    print(tree.gini(rootnode.dataset))
-    print(gain)
-
-
-
-    #rootnode.left_child = lchild1
-    #rootnode.right_child = rchild1
-    #print(len(labels) -1)
-
-    #print(true_row)
+def loadData(dataset):
+    data = pd.read_csv(dataset)
+    return data
 
--- Tree.py
+++ Tree.py
@@ -1,24 +1,66 @@
 import numpy as np
 import Data as d
+import random as r
+
+
+def class_counts(data):
+    counts = {}
+    for row in data:
+        result = row[-1]
+        if result not in counts:
+            counts[result] = 0
+        counts[result] += 1
+    return counts
 
 class Node:
-    def __init__(self, dataset):
-        self.dataset = dataset
-        self.left_child = None
-        self.right_child = None
+    def __init__(self, question, true_branch, false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
 
     def get_data_for_node(self):
         return self.dataset
 
 class Tree:
-    def __init__(self, criterion):
+    def __init__(self, criterion, max_features, max_depth, min_sample_leafs, labels):
         self.criterion = criterion
+        self.max_features = max_features
+        self.max_depth = max_depth
+        self.min_sample_leafs = min_sample_leafs
+        self.labels = labels
+
+    def predict(self, data, node):
+        if isinstance(node, Leaf):
+            return node.predictions
+
+        if node.question.match(data):
+            return self.predict(data, node.true_branch)
+        else:
+            return self.predict(data, node.false_branch)
+
+    def predict_proba(self, counts):
+        total = sum(counts.values()) * 1.0
+        probs = {}
+        for lbl in counts.keys():
+            probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+        return probs
+
+    def fit(self, data):
+        gain, question = self.find_split(data)
+
+        if gain == 0:
+            return Leaf(data)
+
+        true_rows, false_rows = self.partition(data, question)
+
+        true_branch = self.fit(true_rows)
+
+        false_branch = self.fit(false_rows)
+
+        return Node(question, true_branch, false_branch)
 
 
-    def train(self):
-        return
-
-    def find_split(self, data, labels):
+    def find_split(self, data):
         best_gain = 0
         best_question = None
 
@@ -28,12 +70,12 @@ class Tree:
         elif self.criterion == 'Entropy':
             current_uncertainty = self.entropy(data)
 
-        n_features = len(labels) - 1
+        n_features = len(self.labels) - 1
 
         for col in range(n_features):
             values = set([row[col] for row in data])
             for val in values:
-                question = Question(col, val, labels)
+                question = Question(col, val, self.labels)
 
                 true_rows, false_rows = self.partition(data, question)
 
@@ -48,12 +90,18 @@ class Tree:
         return best_gain, best_question
 
     def gini(self, data):
-        rows = Question.class_counts(self, data)
-
+        rows = class_counts(data)
 
         list_of_labels = list(rows.values())
-        n_false_rows = float(list_of_labels.pop())
-        n_true_rows = float(list_of_labels.pop())
+
+        if not list_of_labels:
+            return 0
+        else:
+            n_false_rows = float(list_of_labels.pop())
+            if not list_of_labels:
+                return 0
+            else:
+                n_true_rows = float(list_of_labels.pop())
 
 
 
@@ -63,9 +111,13 @@ class Tree:
             return 1 - (n_true_rows/(n_true_rows + n_false_rows))**2 - (n_false_rows/(n_true_rows + n_false_rows))**2
 
 
-    def entropy(self, true_rows, false_rows):
-        n_true_rows = len(true_rows)
-        n_false_rows = len(false_rows)
+    def entropy(self, data):
+        rows = class_counts(self, data)
+
+
+        list_of_labels = list(rows.values())
+        n_false_rows = float(list_of_labels.pop())
+        n_true_rows = float(list_of_labels.pop())
 
         if n_false_rows == 0 or n_true_rows == 0:
             return 0
@@ -90,7 +142,21 @@ class Tree:
             return current_uncertainty - p * self.gini(true_rows) - (1 - p) * self.gini(false_rows)
 
         elif (self.criterion == 'Entropy'):
-            return current_uncertainty
+            return current_uncertainty - p * self.entropy(true_rows) - p * self.entropy(false_rows)
+
+    def print_tree(self, node, spacing=""):
+
+        if isinstance(node, Leaf):
+            print(spacing + "Predict", node.predictions)
+            return
+
+        print(spacing + str(node.question))
+
+        print(spacing + '--> True:')
+        self.print_tree(node.true_branch, spacing + "  ")
+
+        print(spacing + '--> False:')
+        self.print_tree(node.false_branch, spacing + "  ")
 
 class Question:
     def __init__(self, column, value, labels):
@@ -106,15 +172,6 @@ class Question:
         else:
             return val == self.value
 
-    def class_counts(self, data):
-        counts = {}
-        for row in data:
-            result = row[-1]
-            if result not in counts:
-                counts[result] = 0
-            counts[result] += 1
-        return counts
-
     def __repr__(self):
         condition = "=="
         if self.is_numeric(self.value):
@@ -126,3 +183,32 @@ class Question:
         """Test if a value is numeric."""
         return isinstance(value, int) or isinstance(value, float)
 
+class Leaf:
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+
+if __name__ == '__main__':
+
+
+    data = d.loadData("Datasets/binary/balance-scale.csv")
+
+    labels = d.getLabels(data)
+
+    array = data.values.tolist()
+
+    t = Tree('Gini', None, None, None, labels)
+
+    my_tree = t.fit(array)
+
+    t.print_tree(my_tree)
+
+    testing_data = [
+        [3, 3, 2, 1, 'R'],
+        [5, 1, 3, 4, 'L'],
+        [1, 5, 2, 2, 'L'],
+        [2, 2, 3, 5, 'R'],
+        [1, 3, 3, 2, 'L'],
+    ]
+    for row in testing_data:
+        print("Actual: %s. Predicted: %s" % (row[-1], t.predict_proba(t.predict(testing_data, my_tree))))
